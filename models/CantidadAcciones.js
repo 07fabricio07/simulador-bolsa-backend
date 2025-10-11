6bde7c64563202acf9c01a45620f751b5696c6a2
Fabricio <07fabricio07@gmail.com>
2025-10-10 19:40:38 -0500

    Descripción de los cambios realizados

--- models/CantidadAcciones.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const CantidadAccionesSchema = new mongoose.Schema({
-  cantidad: Number
-});
-
-module.exports = mongoose.model('CantidadAcciones', CantidadAccionesSchema);
