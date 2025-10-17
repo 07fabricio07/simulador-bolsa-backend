10ac48f0e8cc8c1c7610c423c213fd448bd8939a
Fabricio <07fabricio07@gmail.com>
2025-10-16 20:52:59 -0500

    Descripción de los cambios realizados

--- crearUsuarios.js
+++ crearUsuarios.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 const User = require('./models/User');
 
 const usuarios = [
-  { usuario: "jugador1", nombre: "Jugador Uno", password: "apass1qwer", rol: "jugador" },
-  { usuario: "jugador2", nombre: "Jugador Dos", password: "bpass2qwer", rol: "jugador" },
-  { usuario: "jugador3", nombre: "Jugador Tres", password: "cpass3qwer", rol: "jugador" },
-  { usuario: "jugador4", nombre: "Jugador Cuatro", password: "dpass4qwer", rol: "jugador" },
-  { usuario: "jugador5", nombre: "Jugador Cinco", password: "epass5qwer", rol: "jugador" },
-  { usuario: "jugador6", nombre: "Jugador Seis", password: "fpass6qwer", rol: "jugador" },
-  { usuario: "jugador7", nombre: "Jugador Siete", password: "gpass7qwer", rol: "jugador" },
-  { usuario: "jugador8", nombre: "Jugador Ocho", password: "hpass8qwer", rol: "jugador" },
-  { usuario: "jugador9", nombre: "Jugador Nueve", password: "ipass9qwer", rol: "jugador" },
-  { usuario: "jugador10", nombre: "Jugador Diez", password: "jpass10qwer", rol: "jugador" },
-  { usuario: "jugador11", nombre: "Jugador Once", password: "kpass11qwer", rol: "jugador" },
-  { usuario: "jugador12", nombre: "Jugador Doce", password: "lpass12qwer", rol: "jugador" },
-  { usuario: "jugador13", nombre: "Jugador Trece", password: "mpass13qwer", rol: "jugador" },
+  { usuario: "jugador1", nombre: "Jugador 1", password: "pass1", rol: "jugador" },
+  { usuario: "jugador2", nombre: "Jugador 2", password: "pass2", rol: "jugador" },
+  { usuario: "jugador3", nombre: "Jugador 3", password: "pass3", rol: "jugador" },
+  { usuario: "jugador4", nombre: "Jugador 4", password: "pass4", rol: "jugador" },
+  { usuario: "jugador5", nombre: "Jugador 5", password: "pass5", rol: "jugador" },
+  { usuario: "jugador6", nombre: "Jugador 6", password: "pass6", rol: "jugador" },
+  { usuario: "jugador7", nombre: "Jugador 7", password: "pass7", rol: "jugador" },
+  { usuario: "jugador8", nombre: "Jugador 8", password: "pass8", rol: "jugador" },
+  { usuario: "jugador9", nombre: "Jugador 9", password: "pass9", rol: "jugador" },
+  { usuario: "jugador10", nombre: "Jugador 10", password: "pass10", rol: "jugador" },
+  { usuario: "jugador11", nombre: "Jugador 11", password: "pass11", rol: "jugador" },
+  { usuario: "jugador12", nombre: "Jugador 12", password: "pass12", rol: "jugador" },
+  { usuario: "jugador13", nombre: "Jugador 13", password: "pass13", rol: "jugador" },
   { usuario: "admin", nombre: "Administrador", password: "adminpass", rol: "admin" },
 
   // NUEVOS REGISTRADORES
